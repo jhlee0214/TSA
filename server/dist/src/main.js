388e6c846e4115128b14160a793d20130f6889a1
jhlee0214 <67400921+jhlee0214@users.noreply.github.com>
2025-11-09 23:02:19 +1100

    Commit

--- server/dist/src/main.js
+++ server/dist/src/main.js
@@ -3,6 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
 const common_1 = require("@nestjs/common");
+const swagger_1 = require("@nestjs/swagger");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
     app.enableCors();
@@ -14,6 +15,13 @@ async function bootstrap() {
             enableImplicitConversion: true,
         },
     }));
+    const config = new swagger_1.DocumentBuilder()
+        .setTitle('Tasks API')
+        .setDescription('API documentation for the Tasks service')
+        .setVersion('1.0')
+        .build();
+    const document = swagger_1.SwaggerModule.createDocument(app, config);
+    swagger_1.SwaggerModule.setup('api-docs', app, document);
     await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
